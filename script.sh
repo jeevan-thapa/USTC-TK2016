2da35a4d7420b69c7c66596cf0f44e75c37e61da
jeevan-thapa <jeevan.thapa@fusemachines.com>
2021-03-05 16:17:27 +0545

    Initial commit

---
+++ script.sh
@@ -0,0 +1,31 @@
+# Setup mono for Ubuntu-20.04
+
+sudo apt install gnupg ca-certificates
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+echo "deb https://download.mono-project.com/repo/ubuntu stable-focal main" | sudo tee /etc/apt/sources.list.d/mono-official-stable.list
+sudo apt update
+sudo apt install mono-devel
+
+
+
+# Setup powershell
+
+sudo apt-get update
+sudo apt-get install -y wget apt-transport-https software-properties-common
+wget -q https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
+sudo apt-get update
+sudo add-apt-repository universe
+sudo apt-get install -y powershell
+
+
+# Setup fdupes
+
+sudo apt-get install fdupes
+
+
+
+# Download USTC-TFC2016 dataset
+cd 1_Pcap | git clone https://github.com/yungshenglu/USTC-TFC2016.git
+
+
